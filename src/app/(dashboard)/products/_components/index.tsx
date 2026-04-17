c828ebda3d87172b3bcf6a5c74f851cd45a61cd7
digitalart69 <ideashomedecorations@gmail.com>
2026-04-17 00:11:38 +0000

    masmut

--- src/app/(dashboard)/products/_components/index.tsx
+++ src/app/(dashboard)/products/_components/index.tsx
@@ -5,17 +5,7 @@ import { useState, Fragment } from "react";
 import ProductActions from "./ProductActions";
 import ProductFilters from "./ProductFilters";
 import AllProducts from "./products-table";
-import ProductFormSheet from "./form/ProductFormSheet";
 
-export default function Products() {
-  return (
-    <div className="space-y-4">
-      <ProductFormSheet />
-
-      {/* table kamu */}
-    </div>
-  );
-}
 export default function Products() {
   const [rowSelection, setRowSelection] = useState({});
 
@@ -25,14 +15,11 @@ export default function Products() {
         rowSelection={rowSelection}
         setRowSelection={setRowSelection}
       />
-	<div className="space-y-4">
-      <ProductFormSheet />
       <ProductFilters />
       <AllProducts
         rowSelection={rowSelection}
         setRowSelection={setRowSelection}
       />
-	</div>
     </Fragment>
   );
 }
