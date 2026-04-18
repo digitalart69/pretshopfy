9152cfd3da1757d61edf47f629b0ae8b364bc7d7
digitalart69 <ideashomedecorations@gmail.com>
2026-04-18 13:15:02 +0000

    clear

--- src/helpers/formatValidationErrors.ts
+++
@@ -1,9 +0,0 @@
-type FieldErrors = Record<string, string[]>;
-type FlattenedErrors = Record<string, string>;
-
-export function formatValidationErrors(errors: FieldErrors): FlattenedErrors {
-  return Object.entries(errors).reduce((acc, [key, value]) => {
-    acc[key] = value[0];
-    return acc;
-  }, {} as FlattenedErrors);
-}
--- src/types/pagination.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Pagination {
-  limit: number;
-  current: number;
-  items: number;
-  pages: number;
-  next: number | null;
-  prev: number | null;
-}
